a3c62ebdefb0f483bfca7c0eb7ac5e1b949422bd
Natthapoom Insawan <natthapoom654@outlook.com>
2026-02-01 13:49:44 +0700

    update : Event Scene Slot

--- Assets/Scripts/EventScene/EventSceneSlot.cs
+++ Assets/Scripts/EventScene/EventSceneSlot.cs
@@ -1,3 +1,6 @@
+
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,12 +8,29 @@ using static EventSceneManager;
 
 public class EventSceneSlot : MonoBehaviour
 {
+    [Serializable]
+    private class EventSceneIconData
+    {
+        public EventSceneType key;
+        public Sprite IconSprite;
+    }
+
     [Header("References")]
     [SerializeField] private Toggle selectedToggle;
-    [SerializeField] private TextMeshProUGUI eventNameText;
+    [SerializeField] private Image iconImage;
+
+    [Header("Sprite")]
+    [SerializeField] private List<EventSceneIconData> iconDatas;
+
     public void Init(EventSceneType type)
     {
-        eventNameText.text = type.ToString();
+        EventSceneIconData iconData = iconDatas.Find(data => data.key == type);
+
+        if (iconData != null)
+            iconImage.sprite = iconData.IconSprite;
+        else
+            Debug.LogWarning($"No icon with type {type.ToString()}");
+
     }
 
     public void SetToggle(bool value) => selectedToggle.isOn = value;
